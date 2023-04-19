a058c8cd121ba18cc1995abb03eb8e278c025634
Cherubini <127298157+Cherubini@users.noreply.github.com>
2023-04-19 18:21:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -137,3 +137,36 @@ function createInfo(serie) {
     spanInfo.innerHTML=infoNode;
     return spanInfo;
 }
+
+console.log('esercizio, id riunioni meet');
+
+let meetId = /[a-z]{3}-[a-z]{4}-[a-z]{3}/;
+testRegex(meetId, 'ikz-tjrf-igh'); //true
+testRegex(meetId, 'mmf-tire-sgm'); //true
+testRegex(meetId, 'mmf-aaaaaa-sgmaa'); //false
+testRegex(meetId, '___mmf-tire-sgm'); //true
+testRegex(meetId, 'm_f-tire-sgm'); //false
+
+function testRegex(pattern, string) {
+    console.log("testing string '" + string + "': " + pattern.test(string));
+}
+
+console.log('riconoscere la data \n\n');
+let datePattern = /^\d{1,2}\/\d{1,2}\/\d+( [ad]\.c\.)?$/
+testRegex(datePattern, '10/3/2023'); //true
+testRegex(datePattern, '11/10/1992'); //true
+testRegex(datePattern, '5/5/1800 a.c.'); //true
+testRegex(datePattern, '10-03-2002 d.c'); //false
+testRegex(datePattern, '500/10/10000 a.c'); //true (pattern presente da qualche parte all'interno della stringa)
+
+console.log('esercizio: riconoscere un dominio \n\n');
+let domainPattern=/^[a-zA-Z][\w\-]*(\.[a-zA-Z]+)*(:\d+)?$/
+testRegex(domainPattern, 'google.com'); //true
+testRegex(domainPattern, 'localhost:8080'); //true
+testRegex(domainPattern, 'english-.site.co.uk:21'); //true
+testRegex(domainPattern, 'localhost:'); //false
+testRegex(domainPattern, 'personal-home-page.it'); //true
+testRegex(domainPattern, '.page.it'); //false
+testRegex(domainPattern, 'www.my-life-sucks.it'); //false
+
+
